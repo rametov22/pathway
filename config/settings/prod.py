9d4acb0d522f401169886ca47556ce78ddb3d8d4
rametov22 <rametovabdulloh@mail.ru>
2025-03-03 10:28:42 +0500

    Update

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,26 +4,14 @@ ALLOWED_HOSTS = ["www.pthwy.co", "pthwy.co", "localhost"]
 
 CSRF_TRUSTED_ORIGINS = ["https://pthwy.co", "https://www.pthwy.co"]
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "pathway_db",
-#         "USER": "pathway_user",
-#         "PASSWORD": "Rr25808773",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#     }
-# }
-
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "test_db",
-        "USER": "postgres",
-        "PASSWORD": "Rr25808773",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
